f28bbdfc597cd54226e7e4506694e6600793f3b6
Vladislav Supalov <V.Supalov@googlemail.com>
2012-12-07 13:23:18 +0100

    A jquery primer, relative positioning & dragndrop

--- static/js/jquery/game.js
+++ static/js/jquery/game.js
@@ -1,3 +1,16 @@
 function init() {
-	
+	gameBoard = $('<div id="gameBoard"></div>')
+	$('body').append(gameBoard)
+
+	//gameBoard.css("width",BoardWidth) //TODO
+	gameCard = $('<div id="card_1" class="gameCard"></div>')
+ 	gameBoard.append(gameCard)
+
+	gameCard.draggable()
+	gameBoard.draggable()
 }
+
+$(function() {
+	$("#gameBoard").draggable()
+	$(".gameCard").draggable()
+})
