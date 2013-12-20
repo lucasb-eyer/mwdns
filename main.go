e281c7279a170ba4ec8dd5bdae91e226ae57c868
Vladislav Supalov <vladislav@supalov.com>
2013-12-20 17:34:20 +0100

    First step towards Issue #40.

--- main.go
+++ main.go
@@ -26,31 +26,47 @@ const (
     DEFAULT_MAX_PLAYERS = 0
 )
 
-var (
-    addr = flag.String("addr", "localhost:8080", "http service address")
-    homeTempl = utils.CreateAutoTemplate("templates/startView.html", DEV_MODE)
-    gameTempl = utils.CreateAutoTemplate("templates/gameView.html", DEV_MODE)
-    activeGames = make(map[string]*game.Game)
-)
-
-func homeHandler(c http.ResponseWriter, req *http.Request) {
-    homeTempl.Execute(c, utils.CardInformation) //req.Host?
+type GameManager struct {
+    activeGames map[string]*game.Game
 }
 
-// TODO: should be in GameManager
-func cleanGames() {
+func NewGameManager() *GameManager {
+    gm := &GameManager{}
+    //TODO: init channels
+    gm.activeGames = make(map[string]*game.Game)
+
+    return gm
+}
+
+func (gm *GameManager) CreateNewGame(nCards, gameType, maxPlayers, cardType, cardLayout, cardRotation int) (gameId string) {
+    //TODO: there we want to create a game, but do not really care for the exact id
+    //  this might be solved by one single go process, listening for channel requests for new games/for them to be deleted
+    gameId = utils.RndString(IDLEN) //TODO: check for collision
+
+    g := game.NewGame(nCards, gameType, maxPlayers, cardType, cardLayout, cardRotation)
+
+    //TODO channels, as map is not threadsafe (?)
+    gm.activeGames[gameId] = g
+
+    log.Println("New game of type", gameType, "with", nCards, "cards and at most", maxPlayers, "players created: ", gameId)
+    log.Println("The above game's card type is", cardType, "the card layout is", cardLayout, "and their rotation is", cardRotation)
+    go g.Run()
+
+    return
+}
+
+func (gm *GameManager) cleanGames() {
     // Goes over the list of active games and removes those which are over.
     // TODO: highscore? history?
-    for gid, g := range activeGames {
+    for gid, g := range gm.activeGames {
         // Gotta leave the first player some time to join tho!
         if g.Players.Len() == 0 && time.Since(g.Started).Seconds() > 10 {
             log.Println("Removing empty game ", gid)
-            delete(activeGames, gid)
+            delete(gm.activeGames, gid)
         }
     }
 }
 
-// TODO: should be in GameManager
 // called by the gameHandler, when no game id is given or the id looks invalid
 func tryCreateNewGame(w http.ResponseWriter, req *http.Request) {
     // create a new game if no gameid is given
@@ -98,23 +114,29 @@ func tryCreateNewGame(w http.ResponseWriter, req *http.Request) {
         cardRotation = 0
     }
 
-    //TODO: there we want to create a game, but do not really care for the exact id
-    //  this might be solved by one single go process, listening for channel requests for new games/for them to be deleted
-    var gameId = utils.RndString(IDLEN)
-    g := game.NewGame(nCards, gameType, maxPlayers, cardType, cardLayout, cardRotation)
-    activeGames[gameId] = g
-    log.Println("New game of type", gameType, "with", nCards, "cards and at most", maxPlayers, "players created: ", gameId)
-    log.Println("The above game's card type is", cardType, "the card layout is", cardLayout, "and their rotation is", cardRotation)
-    go g.Run()
+    gameId := gameManager.CreateNewGame(nCards, gameType, maxPlayers, cardType, cardLayout, cardRotation)
 
     http.Redirect(w, req, fmt.Sprintf("/game?g=%v", gameId), 303)
+}
+
+
+var (
+    addr = flag.String("addr", "localhost:8080", "http service address")
+    homeTempl = utils.CreateAutoTemplate("templates/startView.html", DEV_MODE)
+    gameTempl = utils.CreateAutoTemplate("templates/gameView.html", DEV_MODE)
+
+    gameManager = NewGameManager()
+)
+
+func homeHandler(c http.ResponseWriter, req *http.Request) {
+    homeTempl.Execute(c, utils.CardInformation) //req.Host?
 }
 
 // TODO: pray that this is not called in multiple threads! It's not safe at all.
 func gameHandler(w http.ResponseWriter, req *http.Request) {
     // This is the "reaper" form of cleanup: cleanup every now and then
     // (i.e. whenever there is a request to /game).
-    cleanGames()
+    gameManager.cleanGames()
 
     gameId := req.URL.Query().Get("g")
     if gameId == "" || len(gameId) != 6 {
@@ -124,7 +146,7 @@ func gameHandler(w http.ResponseWriter, req *http.Request) {
         log.Println("Game", gameId, "requested")
 
         //redirect to the start page if the game does not exist
-        _, ok := activeGames[gameId]
+        _, ok := gameManager.activeGames[gameId]
         if !ok {
             log.Println("Game not found: ", gameId)
             errmsg := "The game you were trying to join (id: <b>" + gameId + "</b>) doesn't exist!"
@@ -139,7 +161,7 @@ func gameHandler(w http.ResponseWriter, req *http.Request) {
 func wsHandler(ws *websocket.Conn) {
     // Get the game we're talking about, if it exists.
     gameId := ws.Request().URL.Query().Get("g")
-    gameInstance, ok := activeGames[gameId]
+    gameInstance, ok := gameManager.activeGames[gameId]
     if !ok {
         log.Println("Websocket request with invalid gameId: ", gameId)
         websocket.Message.Send(ws, `{"msg": "err_gameid", "gid": "`+gameId+`"}`)
